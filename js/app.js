0dd22a00cdbee457d3b608c00c9262fdb56b8e17
hebert-nr <nheb461@outlook.com>
2016-08-22 16:55:45 -0700

    minor updates

--- js/app.js
+++ js/app.js
@@ -17,30 +17,34 @@
 			$scope.$on('$destroy', function() {
 				window.onbeforeunload = undefined;
 			});			
-		});
+	});
+	
+	
 	app.controller('OtherController',function($scope) {
 		$scope.pageChangeHandler = function(num) {
 			console.log('going to page ' + num);
 		};
 	});	
 	
+	
 	app.controller('NavController', function(){
 		this.tab = 1;
-		
 		this.selectTab = function(setTab){
 			this.tab = setTab;
 		};
-		
 		this.isSelected= function(checkTab){
 			return this.tab === checkTab;
 		};
 	});
 		
+		
+		
+		
 	app.config(function($compileProvider){
 		$compileProvider.aHrefSanitizationWhitelist(/^\s*(geo|https?|ftp|mailto|file|ghttps?|ms-appx|x-wmapp0):/);
 	}); 	
 	
-		
+	
 	
 	//filter Multiple...
 	app.filter('filterMultiple',['$filter',function ($filter) {
@@ -83,6 +87,7 @@
 		}
 	}]);
 
+	
 	app.filter('unique', function() {
 		return function(input, key) {
 			var unique = {};
